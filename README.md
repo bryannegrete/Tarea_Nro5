97d29af9a7b4fe2b3c2ff188c7c1d8d680c57729
bryan_negrete <bryanegrete@gmail.com>
2019-01-18 02:48:54 -0300

    Se realiza Tarea Nro5 con la finalidad de conocer y aplicar la extracción información. Se explican y responden los pasos de la Tarea Nro5 en el Readme.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Tarea_Nro5
+﻿# Tarea_Nro5
 Creación de página web y extracción de datos
+
+a) Se confecciona página web "Tarea_Nro5"
+	Se agrega imagen, texto y tabla.
+	según las indicaciones dadas
+	a.1) Tag "img", es un tag que no se cierra.
+
+b) Utilizando programción R, se realizan las siguientes funciones para le extracción de información
+   de páginas en formato HTML.
+	- read_htm => Para leer el archivo HTML
+	- html_nodes => Para extraer irformación
+	- html_text => Para extraer solo el texto en la información extraida
+	- Se realiza limpieza a la información extraida
+	- Se almacena la información en un DataFrame
+
+c) Se extraen datos de la tabla utilizando la función
+	- html_table => Función similar que html_texo, pero esto sirve para los Tag's de "tabla"
+	- Se realiza limpieza a la información extraida
+	- Se almacena la información en un DataFrame
+
+
+d) Se escoge una página web la que se nombró como "Pagina_Tarea_Nro5"
+   -	Se escoge un procedimiento viste en el punto b) o c) y se realiza con la página escogida.
+   -	Se realiza una limpieza a una noticia, por lo que se extrae el texto de ésta.
+   -	Se guarda información en CSV y en un txt.
--- Tarea_Nro5.R
+++ Tarea_Nro5.R
@@ -1,6 +1,10 @@
 # Usando la libreria rvest
 library('rvest')
 
+
+       #Tarea 5 con Pagina Web Creada
+       #Desarrollo de a) b) y c)
+
 #Inicializando la viable PaginaWeb con el nombre de la pagina
 PaginaWeb<-"Tarea_Nro5.html"
 
@@ -23,6 +27,12 @@ print(Tabla)
 #pasando la info a tabla
 TablaWeb<-html_table(Tabla)[[1]]
 print(TablaWeb)
+
+#Pasando informacion a un data frame
+DataFrameTabla<-as.data.frame(TablaWeb)
+
+#Almacenando la informacion en CSV
+write.csv(DataFrameTabla, file = "TablaNoticia.csv")
 
 #Eliminando los \n, comillas("), puntos(.) y comas(,) del texto
 Parrafo<-gsub("\n","",Parrafo)
@@ -67,3 +77,115 @@ MinusculaPalabras<-tolower(EspacioPalabras)
 ContarPalabras<-unlist(MinusculaPalabras)
 TablaPalabras<-table(ContarPalabras)
 DataFramePalabras<-as.data.frame(TablaPalabras)
+write.csv(DataFramePalabras, file = "PalabrasNoticia.csv")
+
+
+#Grafico con los datos
+
+#Limpiar tabla
+TablaWeb$`Precio de Cierre` <- gsub("\\$","",TablaWeb$`Precio de Cierre`)
+TablaWeb$`Precio de Cierre` <- gsub("[.]","",TablaWeb$`Precio de Cierre`)
+TablaWeb$`Precio de Cierre` <- as.numeric(gsub(",",".",TablaWeb$`Precio de Cierre`))
+
+#Graficando los precios de acciones
+library('ggplot2')
+
+# respecto al precio
+TablaWeb %>%
+  ggplot() +
+  aes(x = Empresa, y = `Precio de Cierre`) +
+  geom_bar(stat="identity")
+
+# Gráfico boxplot diferenciado por producto
+TablaWeb %>%
+  ggplot() +
+  geom_boxplot(aes(x = Empresa, y = `Precio de Cierre`)) +
+  theme_bw()
+
+
+      #Tarea 5 con Pagina Web Creada
+      #Desarrollo de d)
+
+PageEmol<-"Pagina_Tarea_Nro5.html"
+ReadEmol<-read_html(PageEmol)
+print(ReadEmol)
+
+#Extrayendo Titulo y Bajada de la Noticia
+TituloBajadaEmol<-html_nodes(ReadEmol,".cont_new_detalle_noti")
+print(TituloBajadaEmol)
+
+TituloEmol<-html_nodes(TituloBajadaEmol,"h1")
+print(TituloEmol)
+
+Titulo <- html_text(TituloEmol)
+print(Titulo)
+
+Titulo <- gsub("\n","",Titulo)
+Titulo <- gsub("\r","",Titulo)
+Titulo <- gsub("[.]","",Titulo)
+Titulo <- gsub("\"","",Titulo)
+
+print(Titulo)
+
+BajadaEmol<-html_nodes(TituloBajadaEmol,"h2")
+print(BajadaEmol)
+
+Bajada <- html_text(BajadaEmol)
+print(Bajada)
+
+Bajada <- gsub("\n","",Bajada)
+Bajada <- gsub("\r","",Bajada)
+Bajada <- gsub("[.]","",Bajada)
+Bajada <- gsub("\"","",Bajada)
+
+print(Bajada)
+
+#Juntamos los parrafos de la bajada en texto completo
+BajadaCompleta<-""
+for(i in 1:length(Bajada))
+  BajadaCompleta<-paste(BajadaCompleta," ",Bajada[i])
+
+print(BajadaCompleta)
+
+BajadaCompleta <- gsub("\n","",BajadaCompleta)
+BajadaCompleta <- gsub("\r","",BajadaCompleta)
+BajadaCompleta <- gsub("[.]","",BajadaCompleta)
+BajadaCompleta <- gsub("\"","",BajadaCompleta)
+BajadaCompleta <- gsub(",","",BajadaCompleta)
+BajadaCompleta <- gsub(":","",BajadaCompleta)
+
+print(BajadaCompleta)
+
+#Extrayendo Cuerpo de la Noticia
+CuerpoEmol<-html_nodes(ReadEmol,".EmolText")
+print(CuerpoEmol)
+
+Cuerpo <- html_text(CuerpoEmol)
+print(Cuerpo)
+
+Cuerpo <- gsub("\n","",Cuerpo)
+Cuerpo <- gsub("\r","",Cuerpo)
+Cuerpo <- gsub("[.]","",Cuerpo)
+Cuerpo <- gsub("\"","",Cuerpo)
+Cuerpo <- gsub(",","",Cuerpo)
+
+print(Cuerpo)
+
+# Separando las palabras por espacio
+EspacioEmol <- strsplit(Cuerpo," ")[[1]]
+
+# Pasando todas las palabras a minusculas
+EspacioEmol <- tolower(EspacioEmol)
+
+# Contando palabras
+UnlistEmol<-unlist(EspacioEmol)
+TablaEmol<-table(UnlistEmol)
+
+# Pasando la informacion a un data frame
+DfCuerpoEmol<- as.data.frame(TablaEmol)
+
+# Almacenando la informacion en CSV
+write.csv(DfCuerpoEmol, file="PalabrasEmol.csv")
+
+# o en un txt
+write.table(DfCuerpoEmol, file="PalabrasEmol.txt")
